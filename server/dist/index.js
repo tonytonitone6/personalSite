7a81499cf9de245f4d68991cbb57ae54ef8fda26
stan <tonytonitone6@gmail.com>
2019-07-17 09:52:43 +0800

    add data

---
+++ server/dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+
+var _graphql = require("graphql");
+
+console.log("123");
---
+++ server/src/schema/types/Post.js
@@ -0,0 +1,31 @@
+import {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLList
+} from "graphql";
+
+import { Author } from "./Author";
+import { fakeDatabase } from "../../FakeDatabase";
+
+export const Post = new GraphQLObjectType({
+  name: "Post",
+  description: "All details of a blog post",
+  fields: () => ({
+    id: { type: GraphQLInt },
+    title: { type: GraphQLString },
+    content: { type: GraphQLString },
+    author: {
+      type: Author,
+      resolve: post => {
+        return fakeDatabase.getAuthor(post.author);
+      }
+    },
+    comments: {
+      type: new GraphQLList(Comment),
+      resolve: post => {
+        return fakeDatabase.getCommentsForPost(post.id);
+      }
+    }
+  })
+});

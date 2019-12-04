c85a84387272fda12b2e94e798e32c6bc0cd833f
mao <tonytonitone6@gmail.com>
2019-12-04 18:03:24 +0800

    add content-type

--- server.js
+++ server.js
@@ -8,10 +8,18 @@ const app = express()
 app.use(history())
 
 app.get('*.js', (req, res, next) => {
-  req.url = `${req.url}.gz`;
-  res.set('Content-Encoding', 'gzip');
-  next();
-});
+  req.url = `${req.url}.gz`
+  res.set('Content-Encoding', 'gzip')
+  res.set('Content-Type', 'text/javascript')
+  next()
+})
+
+app.get('*.css', (req, res, next) => {
+  req.url = `${req.url}.gz`
+  res.set('Content-Encoding', 'gzip')
+  res.set('Content-Type', 'text/css')
+  next()
+})
 
 app.use(express.static(join(__dirname, 'dist')))
 
